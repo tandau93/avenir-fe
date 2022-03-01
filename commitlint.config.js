5846e6dc20b8f56c37f048c6369169ea6c1b98d1
daupham <dau.pham@bbv.vn>
2022-03-01 17:24:56 +0700

    Update: eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,11 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-case': [0, 'always', 'start-case'],
-    'type-enum': [
-      2,
-      'always',
-      ['Fix', 'Chore', 'New', 'Docs', 'Breaking', 'Upgrade', 'Update', 'Build'],
-    ],
+    'type-enum': [2, 'always', ['Fix', 'New', 'Docs', 'Upgrade', 'Update']],
     'subject-case': [0, 'always', 'start-case'],
   },
 };
